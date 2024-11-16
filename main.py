94d004105e83b82609c62501b206aab941305c17
José Balbuena <45837292+JoseBalbuena181096@users.noreply.github.com>
2024-11-16 14:42:53 -0600

    maquetacion del sistema

---
+++ main.py
@@ -0,0 +1,34 @@
+
+"""
+Atributos de un experimento: 
+nombre, fecha, tipo y resultados númericos
+"""
+
+def agregarExperimento():
+    """ Permite agregar un nuevo experimento con sus atributos """
+    pass
+
+def visualizarExperimentos():
+    """ Permite agregar experimentos """
+    pass
+
+def calcularEstadisticas():
+    """ Calcular estadisticas basicas (promedios, maximos y minimos) de un experimentos """
+    pass
+
+def generaInforme():
+    """ Generar un informe resumen de los experimentos y sus estadisticas """
+    pass
+
+def mostrarMenu():
+    """ Muestra el menu principal del programa """
+    pass
+
+
+def main():
+    """ Funcion que controla el flujo general del sistema """
+    pass
+
+
+if __name__ == "__main__":
+    main()
